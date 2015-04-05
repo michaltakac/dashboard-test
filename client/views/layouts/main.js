bd5155f9073aaa3e9cd57c1982db689c91934e13
Michal Takac <takacmichal92@gmail.com>
2015-04-05 22:27:18 +0200

    added collapse click events on boxes

--- client/views/layouts/main.js
+++ client/views/layouts/main.js
@@ -6,5 +6,26 @@ Template['MainLayout'].events({
 	'click .footer-tools': function () {
 		scrollTop();
 		event.preventDefault;
-	}
+	},
+
+	/*-----------------------------------------------------------------------------------*/
+	/* Box tools
+	/*-----------------------------------------------------------------------------------*/
+	//Collapse
+	'click .box .tools .collapse, click .box .tools .expand': function (event) {
+		var self = event.currentTarget;
+        var el = jQuery(self).parents(".box").children(".box-body");
+        if (jQuery(self).hasClass("collapse")) {
+			jQuery(self).removeClass("collapse").addClass("expand");
+            var i = jQuery(self).children(".fa-chevron-up");
+			i.removeClass("fa-chevron-up").addClass("fa-chevron-down");
+            el.slideUp(200);
+        } else {
+			jQuery(self).removeClass("expand").addClass("collapse");
+            var i = jQuery(self).children(".fa-chevron-down");
+			i.removeClass("fa-chevron-down").addClass("fa-chevron-up");
+            el.slideDown(200);
+        }
+        event.preventDefault();
+    }
 });
