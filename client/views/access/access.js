0550e11bb9bb90199b01c09cd8bc7740239618b6
Michal Takac <takacmichal92@gmail.com>
2015-04-05 22:45:29 +0200

    added click events to switch between login/register/forgot pass

--- client/views/access/access.js
+++ client/views/access/access.js
@@ -2,6 +2,22 @@ Template['Access'].helpers({
 	
 });
 
+var swapScreen = function (id) {
+	$('.visible').removeClass('visible animated fadeInUp');
+	$('#'+id).addClass('visible animated fadeInUp');
+}
+
 Template['Access'].events({
-	
+	'click .register-link': function(event) {
+		swapScreen('register');
+		return false;
+	},
+	'click .forgot-link': function(event) {
+		swapScreen('forgot');
+		return false;
+	},
+	'click .login-link': function(event) {
+		swapScreen('login');
+		return false;
+	}
 });
