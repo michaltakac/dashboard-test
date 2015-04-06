acc2ea6edc1d196e72dc271968f4a41b2b37fcad
Michal Takac <takacmichal92@gmail.com>
2015-04-07 01:47:19 +0200

    added determineEmail

--- server/startup/accounts-creation.js
+++ server/startup/accounts-creation.js
@@ -1,3 +1,28 @@
+var determineEmail = function(user) {
+	var emailAddress, services;
+	if (user.emails) {
+		return emailAddress = user.emails[0].address;
+	} else if (user.services) {
+		services = user.services;
+		return emailAddress = (function() {
+			switch (false) {
+				case !services.facebook:
+					return services.facebook.email;
+				case !services.github:
+					return services.github.email;
+				case !services.google:
+					return services.google.email;
+				case !services.twitter:
+					return null;
+				default:
+					return null;
+			}
+		})();
+	} else {
+		return null;
+	}
+};
+
 Accounts.onCreateUser( function(options, user) {
 	var userData = {
 		email: determineEmail(user),
