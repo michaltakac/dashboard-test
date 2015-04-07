ca2205b1484e3e5195ff9a772043b94f05d7a754
Michal Takac <takacmichal92@gmail.com>
2015-04-07 10:20:46 +0200

    added userIdentity global helper

--- client/lib/spacebarHelpers.js
+++ client/lib/spacebarHelpers.js
@@ -25,3 +25,32 @@ Template.registerHelper('debug', function (optionalValue) {
 Template.registerHelper('constant', function (what) {
   return Meteor.App[what.toUpperCase()];
 });
+
+Template.registerHelper('userIdentity', function(userId) {
+  var getService, getUser, services;
+  getUser = Meteor.users.findOne({
+    _id: userId
+  });
+  if (getUser.emails) {
+    return getUser.emails[0].address;
+  } else if (getUser.services) {
+    services = getUser.services;
+    getService = (function() {
+      switch (false) {
+        case !services.facebook:
+          return services.facebook.email;
+        case !services.github:
+          return services.github.email;
+        case !services.google:
+          return services.google.email;
+        case !services.twitter:
+          return services.twitter.screenName;
+        default:
+          return false;
+      }
+    })();
+    return getService;
+  } else {
+    return getUser.profile.name;
+  }
+});
