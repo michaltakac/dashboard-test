db11886f7cec5df371d6de2844d8706c05517296
Michal Takac <takacmichal92@gmail.com>
2015-04-07 02:06:00 +0200

    added sendWelcomeEmail method

--- server/email/validation.js
+++ server/email/validation.js
@@ -23,5 +23,24 @@ Meteor.methods({
       }
     });
     return validateEmail.wait();
+  },
+  sendWelcomeEmail: function(userData) {
+    var emailTemplate;
+    check(userData, {
+      email: String,
+      name: String
+    });
+    SSR.compileTemplate('welcomeEmail', Assets.getText('email/welcome-email.html'));
+    emailTemplate = SSR.render('welcomeEmail', {
+      email: userData.email,
+      name: userData.name !== "" ? userData.name : null,
+      url: "http://localhost:3000"
+    });
+    return Email.send({
+      to: userData.email,
+      from: "The Meteor Chef - Demo <demo@themeteorchef.com>",
+      subject: "Welcome aboard, team matey!",
+      html: emailTemplate
+    });
   }
 });
